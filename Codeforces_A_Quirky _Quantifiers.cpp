3b85b4d81ad50412f76d4082aa6afaa1eba8646a
Nahid Hasan <30774439+nahid597@users.noreply.github.com>
2018-04-02 00:48:22 +0600

    This is codeforces problem A solution.

---
+++ Codeforces_A_Quirky _Quantifiers.cpp
@@ -0,0 +1,18 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+
+    cin >> n;
+
+    if(n % 2 == 0)
+      printf("0\n");
+      else
+        printf("1\n");
+
+
+    return 0;
+}
